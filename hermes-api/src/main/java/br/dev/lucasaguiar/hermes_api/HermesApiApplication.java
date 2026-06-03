721a466506c2da20a805a3389804a70f34083c6a
Lucas Aguiar <lucasbarbary@gmail.com>
2026-06-02 23:48:45 -0500

    Cria projeto da API com Java 11 e Spring Boot 2.7

---
+++ hermes-api/src/main/java/br/dev/lucasaguiar/hermes_api/HermesApiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lucasaguiar.hermes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HermesApiApplication.class, args);
+	}
+
+}
---
+++ hermes-api/src/test/java/br/dev/lucasaguiar/hermes_api/HermesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lucasaguiar.hermes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
