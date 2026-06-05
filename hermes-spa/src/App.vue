733c3fb468e07e84448de2ee08405b99a9469ce9
Lucas Aguiar <lucasbarbary@gmail.com>
2026-06-04 22:33:40 -0500

    Instala biblioteca de components Naive UI

--- hermes-spa/src/App.vue
+++ hermes-spa/src/App.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import {
+  NConfigProvider,
+  NMessageProvider,
+  NDialogProvider,
+  NNotificationProvider,
+  NLoadingBarProvider
+} from 'naive-ui'
 
 </script>
 
 <template>
-  <div>
-    <p class="text-red-600 font-extrabold">Hello</p>
-  </div>
+  <n-config-provider>
+    <n-loading-bar-provider>
+      <n-dialog-provider>
+        <n-notification-provider>
+          <n-message-provider>
+            <router-view />
+          </n-message-provider>
+        </n-notification-provider>
+      </n-dialog-provider>
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
